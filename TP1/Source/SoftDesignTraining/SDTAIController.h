4c84f2bfe4868472ce344c2e8ce0be68f5384e37
AlexxPC <alexandre.provostcardin@gmail.com>
2025-09-27 19:59:23 -0400

    Parties 1-7

--- TP1/Source/SoftDesignTraining/SDTAIController.h
+++ TP1/Source/SoftDesignTraining/SDTAIController.h
@@ -15,5 +15,32 @@ class SOFTDESIGNTRAINING_API ASDTAIController : public AAIController
 {
     GENERATED_BODY()
 public:
+    FVector CurrentVelocity;
+    FVector LastAvoidanceDirection;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float Acceleration = 500.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxSpeed = 600.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float DetectionDistance = 300.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float FloorDetectionDistance = 400.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float PlayerDetectionRadius = 500.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float RotationSpeed = 15.f;
+
     virtual void Tick(float deltaTime) override;
+    
+private:
+    void MovePawn(float deltaTime);
+    void AvoidObstacles(float deltaTime);
+    void ChaseCollectible(float deltaTime);
+    void ChaseOrFleePlayer(float deltaTime);
 };
