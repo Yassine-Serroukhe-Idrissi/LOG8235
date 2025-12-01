f04e060367f49e0c000466cf99fc274b20f68889
Yassine Serroukhe Idrissi <yassine.serroukhe@gmail.com>
2025-11-30 21:12:23 -0500

    Optimization question 4

--- TP3/Source/SoftDesignTraining/SDTBaseAIController.h
+++ TP3/Source/SoftDesignTraining/SDTBaseAIController.h
@@ -1,24 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "AIController.h"
 #include "SDTBaseAIController.generated.h"
 
-/**
- *
- */
+
 UCLASS()
 class SOFTDESIGNTRAINING_API ASDTBaseAIController : public AAIController
 {
     GENERATED_BODY()
 
 public:
-    ASDTBaseAIController(const FObjectInitializer &ObjectInitializer = FObjectInitializer::Get());
+    ASDTBaseAIController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
     virtual void Tick(float deltaTime) override;
     bool m_ReachedTarget;
-    void TickRate();
+    void UpdateTickRate();
+
+    bool IsVisibleInCameraView();
+
+    static float TotalTimeBudgetUsed;
+    static const float MaxTimeBudgetPerFrame;
+    static int32 AgentsUpdatedThisFrame;
 
 protected:
     virtual void RotationUpdate(float deltaTime) {};
@@ -28,4 +30,7 @@ private:
     virtual void GoToBestTarget(float deltaTime) {};
     virtual void UpdatePlayerInteraction(float deltaTime) {};
     virtual void ShowNavigationPath() {};
+
+    bool bWasVisibleLastFrame;
+    bool bShouldTickThisFrame;
 };
